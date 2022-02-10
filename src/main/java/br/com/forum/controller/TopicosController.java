6c6868ac44be2a5bb1cf5b55ec107b4ce1bc9aef
Lelton Borges <leltondev@gmail.com>
2022-02-09 21:09:43 -0300

    Project

---
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -0,0 +1,13 @@
+package br.com.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    public ? lista(){
+
+    }
+}
