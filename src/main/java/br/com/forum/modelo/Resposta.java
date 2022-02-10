4d85b1856123784ee8d24d005d83e677451f0326
Lelton Borges <leltondev@gmail.com>
2022-02-09 22:04:16 -0300

    Config Spring Data JPA

--- src/main/java/br/com/forum/modelo/Resposta.java
+++ src/main/java/br/com/forum/modelo/Resposta.java
@@ -1,15 +1,24 @@
 package br.com.forum.modelo;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
+
+	public Resposta() {
+	}
 
 	@Override
 	public int hashCode() {
