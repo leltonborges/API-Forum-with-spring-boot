2ce4d3b0e876f4d50964e92bbe03dd1a27f533c9
Lelton Borges <leltondev@gmail.com>
2022-02-09 21:25:02 -0300

    Bean ModelMapper

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -1,13 +1,21 @@
 package br.com.forum.controller;
 
+import br.com.forum.modelo.Curso;
+import br.com.forum.modelo.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    public ? lista(){
-
+    @ResponseBody
+    public List<Topico> lista(){
+        Topico topico = new Topico("Duvida", "abc", new Curso("Spring", "Back-end"));
+        return Arrays.asList(topico, topico, topico, topico);
     }
 }
